bccc3f88147133d1c2e430036d4e31337992f64d
Ravikiran Bhat <rbh@container-xchange.com>
2023-12-23 16:51:36 +0100

    exclude DataSourceAutoConfiguration

--- coding-challenge-sql-master/src/main/java/exercise/CodingChallengeSQL.java
+++ coding-challenge-sql-master/src/main/java/exercise/CodingChallengeSQL.java
@@ -2,8 +2,9 @@ package exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CodingChallengeSQL {
     public static void main(String[] args) {
         SpringApplication.run(CodingChallengeSQL.class, args);
